155aaaeb765da3c0d55799f15722afd976128226
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-10 17:47:01 +0100

    Cloned repo.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <h1 class="text-success ms-3 py-2">
-        <a class="header-link" href="{{ route("admin.projects.index")}}">Home Index</a>
+        <a class="header-link" href="{{ route("admin.projects.index")}}">Home Index 1°relation</a>
         <a class="btn btn-sm btn-primary px-2 ms-3" href="{{ route("admin.projects.create")}}">Crea Nuovo Progetto</a>
     </h1>
 </header>
