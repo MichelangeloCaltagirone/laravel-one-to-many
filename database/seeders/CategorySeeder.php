7dbfb8f0fb835b6f017ba5646fd10cdceb54af05
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-11 18:17:48 +0100

    Runned the CategorySeeder and updated the db.

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categoryNames = [
+            "Scienza",
+            "Attualità",
+            "Giornalismo",
+            "Sport",
+            "Economia",
+            "Cultura"
+        ];
+
+        foreach($categoryNames as $name) {
+            $newCategory = new Category();
+            $newCategory->name = $name;
+            $newCategory->save();
+        };
     }
 }
